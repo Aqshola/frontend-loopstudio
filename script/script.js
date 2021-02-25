1521c0e217b866166a1a8bbe4dd6f31ec958cafd
Aqshola <aqsholafid2@gmail.com>
2021-02-25 20:45:15 +0700

    finishing layout for desktop

--- script/script.js
+++ script/script.js
@@ -2,5 +2,5 @@ const gallery_img = document.querySelectorAll(".gallery-img");
 
 gallery_img.forEach((img) => {
   console.log(img.getAttribute("data-gallery-img"));
-  img.style.backgroundImage = `url("${img.getAttribute("data-gallery-img")}")`;
+  img.style.background = `url("${img.getAttribute("data-gallery-img")}")`;
 });
